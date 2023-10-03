531d2a74dfe46748b6e62b7daabf5886164f3f6d
Darioorg10 <dricgar1103@g.educaand.es>
2023-10-03 09:33:21 +0200

    Ejercicio 4 terminado y 5 empezado

--- Curso23_24/Ejercicios_Strings/ej4.php
+++ Curso23_24/Ejercicios_Strings/ej4.php
@@ -1,3 +1,66 @@
+<?php
+
+const VALOR = array("M" => 1000, "D" => 500, "C" => 100, "L" => 50,
+"X" => 10, "V" => 5, "I" => 1);
+
+function letras_bien($texto){
+    $bien = true;
+    for ($i=0; $i < strlen($texto); $i++) {
+        if (!isset(VALOR[$texto[$i]])) { // Si no existe algo del texto en las constantes
+            $bien = false;
+            break;
+        }
+    }
+    return $bien;
+}
+
+function orden_bueno($texto){
+    $bien = true;
+    for ($i=0; $i < strlen($texto)-1; $i++) {
+        if(VALOR[$texto[$i]] < VALOR[$texto[$i+1]]){ // Tienen que ir en orden creciente
+            $bien = false;
+            break;
+        }
+    }
+    return $bien;
+}
+
+function repite_bien($texto){
+
+    $veces["I"] = 4; // Es hasta el 5000
+    $veces["V"] = 1;
+    $veces["X"] = 4;
+    $veces["L"] = 1;
+    $veces["C"] = 4;
+    $veces["D"] = 1;
+    $veces["M"] = 4;
+
+    $bien = true;
+    for ($i=0; $i < strlen($texto); $i++) {
+        $veces[$texto[$i]]--;
+        if ($veces[$texto[$i]] == -1) {
+            $bien = false;
+            break;
+        }
+    }
+    return $bien;
+}
+
+function es_romano($texto){
+
+
+    return letras_bien($texto) && orden_bueno($texto) && repite_bien($texto);
+}
+
+if (isset($_POST["btnEnviar"])) { // Comprobamos errores
+    $texto = trim($_POST["num"]);
+    $texto_m = strtoupper($texto);
+
+    $err_form = $texto == "" || !es_romano(strtoupper($texto_m));
+    
+}
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -14,7 +77,7 @@
         p{padding:5px}
         #tit1{text-align:center}
         #tit2{text-align:center}
-        .falloFra{color:red}        
+        .falloNum{color:red}        
 
     </style>
 </head>
@@ -28,21 +91,19 @@
         <p><label for="pri">Número:</label>
             <input type="text" name="num" id="num" value="<?php if(isset($_POST["btnEnviar"])) echo $_POST['num']?>">            
             <span class="falloNum">
-                <?php 
-                    if (isset($_POST["btnEnviar"])) {
-                        $err_num_vacio = trim($_POST["num"]) == "";                        
-                        $err_form = $err_num_vacio;
-                        if ($err_num_vacio) {
+                <?php                                         
+                    if (isset($_POST["btnEnviar"]) && $err_form) {
+                        if ($texto == "") {
                             echo "Campo obligatorio";
+                        } else {
+                            echo "No has escrito un número romano correcto";
                         }
                     }
-                    
-                    
                 ?>
             </span>            
         </p>      
 
-        <p><input type="submit" name="btnEnviar" value="Comprobar"></p>
+        <p><input type="submit" name="btnEnviar" value="Convertir"></p>
 
     </form>
         
@@ -57,26 +118,13 @@
                 <h1 id="tit2">romanos a árabes - Resultado</h1>
             
             <?php            
-
-            $palabraRegular = strtoupper(trim($_POST["fra"])); // Las ponemos sin espacio y mayúsculas
-            $palabraBien = str_replace(" ", "", $palabraRegular);            
-
-            function esPalindroma($p){                
-                
-                for ($i=0; $i < strlen($p)/2; $i++) {
-                    if ($p[$i] != $p[strlen($p) - 1 - $i]) { // Vamos mirando si la primera y la última posición son iguales
-                        return false;
-                    }
+                                    
+                $res = 0;
+                for ($i=0; $i < strlen($texto_m); $i++) { 
+                    $res += VALOR[$texto_m[$i]];
                 }
-                return true;
-            }            
-            
-            if (esPalindroma($palabraBien)) {
-                echo "<p>La frase ".$_POST['fra']." es palíndroma</p>";
-            } else {
-                echo "<p>La frase ".$_POST['fra']." no es palíndroma</p>";
-            }
 
+                echo "<p>El número romano: $texto pasado a cifras árabes es: $res</p>";
 
             ?>
             </div>
---
+++ Curso23_24/Ejercicios_Strings/ej5.php
@@ -0,0 +1,138 @@
+<?php
+
+const VALOR = array("M" => 1000, "D" => 500, "C" => 100, "L" => 50,
+"X" => 10, "V" => 5, "I" => 1);
+
+function letras_bien($texto){
+    $bien = true;
+    for ($i=0; $i < strlen($texto); $i++) {
+        if (!isset(VALOR[$texto[$i]])) { // Si no existe algo del texto en las constantes
+            $bien = false;
+            break;
+        }
+    }
+    return $bien;
+}
+
+function orden_bueno($texto){
+    $bien = true;
+    for ($i=0; $i < strlen($texto)-1; $i++) {
+        if(VALOR[$texto[$i]] < VALOR[$texto[$i+1]]){ // Tienen que ir en orden creciente
+            $bien = false;
+            break;
+        }
+    }
+    return $bien;
+}
+
+function repite_bien($texto){
+
+    $veces["I"] = 4;
+    $veces["V"] = 1;
+    $veces["X"] = 4;
+    $veces["L"] = 1;
+    $veces["C"] = 4;
+    $veces["D"] = 1;
+    $veces["M"] = 4;
+
+    $bien = true;
+    for ($i=0; $i < strlen($texto); $i++) {
+        $veces[$texto[$i]]--;
+        if ($veces[$texto[$i]] == -1) {
+            $bien = false;
+            break;
+        }
+    }
+    return $bien;
+}
+
+function es_romano($texto){
+
+
+    return letras_bien($texto) && orden_bueno($texto) && repite_bien($texto);
+}
+
+if (isset($_POST["btnEnviar"])) { // Comprobamos errores
+    $texto = trim($_POST["num"]);
+    $texto_m = strtoupper($texto);
+
+    $err_form = $texto == "" || !es_romano(strtoupper($texto_m));
+    
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 5</title>
+    <style>
+        .formulario{background-color:lightblue; border:3px solid black}
+        .resultado{
+                    background-color:lightgreen; 
+                    border:3px solid black;
+                    margin-top:2%;
+                  }
+        p{padding:5px}
+        #tit1{text-align:center}
+        #tit2{text-align:center}
+        .falloNum{color:red}        
+
+    </style>
+</head>
+<body>    
+
+    <div class="formulario">
+    
+    <form action="ej5.php" method="post">
+    <h1 id="tit1">árabes a romanos - Formulario</h1>
+        <p>Dime un número en números árabes y lo convertiré en números romanos</p>
+        <p><label for="pri">Número:</label>
+            <input type="text" name="num" id="num" value="<?php if(isset($_POST["btnEnviar"])) echo $_POST['num']?>">            
+            <span class="falloNum">
+                <?php                                         
+                    if (isset($_POST["btnEnviar"]) && $err_form) {
+                        if ($texto == "") {
+                            echo "Campo obligatorio";
+                        } else {
+                            echo "No has escrito un número en cifras árabes correcto";
+                        }
+                    }
+                ?>
+            </span>            
+        </p>      
+
+        <p><input type="submit" name="btnEnviar" value="Convertir"></p>
+
+    </form>
+        
+
+    </div>
+
+    <?php 
+
+        if (isset($_POST["btnEnviar"]) && !$err_form) {
+            ?>
+            <div class="resultado">
+                <h1 id="tit2">árabes a romanos - Resultado</h1>
+            
+            <?php            
+                                    
+                $res = 0;
+                for ($i=0; $i < strlen($texto_m); $i++) {
+                    $res += VALOR[$texto_m[$i]];
+                }
+
+                echo "<p>El número árabe: $texto pasado a números romanos es: $res</p>";
+
+            ?>
+            </div>
+            <?php
+
+        }
+
+    ?>
+
+</body>
+</html>
