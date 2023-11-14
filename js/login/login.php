e846c5c74d704469ede8f00fa03ddc4ed40112f0
Darioorg10 <dricgar1103@g.educaand.es>
2023-11-14 12:50:29 +0100

    Algunos ejercicios javascript

---
+++ js/login/login.php
@@ -0,0 +1,13 @@
+<?php
+$param1 = $_REQUEST["usuario"];
+$param2 = $_REQUEST["pwd"];
+$resultado = "";
+     
+    if ($param1 !== "admin" || $param2 !== "1234") {
+        $resultado = "USUARIO NO VÁLIDO";            
+    } else {
+        $resultado = "USUARIO VÁLIDO";
+    }
+        echo $resultado;        
+           
+?>
