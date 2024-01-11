eeb94fdb0bad63e31d9bb7c6c2428bd371a4c054
Darioorg10 <darioorg1@g.educaand.es>
2024-01-11 14:31:58 +0100

    Teoría de PDO explicada

---
+++ Curso23_24/TEMA_5_POO/Relacion_ejercicios/Ejercicio7/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 7 POO</title>
+</head>
+<body>
+    <h1>Ejercicio 7 POO</h1>
+    <?php
+
+        require "Pelicula.php";
+
+        $pelicula1 = new Pelicula("Star wars", 1995, "George Lucas", false, 11.99, "8/1/2024");
+
+    ?>
+</body>
+</html>
